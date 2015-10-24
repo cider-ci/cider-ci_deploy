60fe9a5c3d9a3fd40198c8a898dc1669f2cb18f8
Thomas Schank <DrTom@schank.ch>
2015-10-24 09:33:29 +0200

    Fix UI Memory Settings, Install graphviz

--- roles/lein-service/templates/start.sh
+++ roles/lein-service/templates/start.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-cd {{lein_service_app_dir}} \
-&& export JVM_OPTS="-Xmx{{lein_service_xmx_value}}" \
-&& lein with-profile production trampoline run >> {{lein_service_log_dir}}/console.log 2>&1
+#!/usr/bin/env bash
+set -eux
+cd {{lein_service_app_dir}}
+export LEIN_FAST_TRAMPOLINE=yes
+export JVM_OPTS="-Xmx{{lein_service_xmx_value}}"
+lein with-profile production trampoline run \
+    >> {{lein_service_log_dir}}/console.log 2>&1
