ccd7f8d32cadca2ff19b797ba5335552b9d86b3d
Thomas Schank <DrTom@schank.ch>
2015-11-25 19:52:44 +0100

    Improve default memory settings

--- server/roles/user-interface/templates/start.sh
+++ server/roles/user-interface/templates/start.sh
@@ -13,7 +13,7 @@ export RAILS_RELATIVE_URL_ROOT={{web_sub_path}}{{web_ui_prefix}}
 export JVM_OPTS="-Xmx{{user_interface_xmx_value}}"
 # it seems that the above is ignored,
 # we could either invoke `jruby -J-Xmx4G` or set JRUBY_OPTS
-export JRUBY_OPTS="-J-Xms{{user_interface_xmx_value}}  -J-Xmx{{user_interface_xmx_value}}"
+export JRUBY_OPTS="-J-Xms{{user_interface_xms_value}}  -J-Xmx{{user_interface_xmx_value}}"
 
 bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:8880 \
   >> {{user_interface_log_dir}}/console.log 2>&1
