c002adc91ef75b4afd4b2cdd86ae6fff3ebbe5ca
Thomas Schank <DrTom@schank.ch>
2015-12-14 11:30:29 +0100

    Update "ZHdK Intern" env; update windows deployment

---
+++ executor-windows/roles/compile/files/jar.ps1
@@ -0,0 +1,2 @@
+cd "C:\cider-ci\executor"
+cmd.exe /C "C:\cider-ci\bin\lein.bat uberjar"
--- executor-windows/roles/compile/files/uberjar.ps1
+++ executor-windows/roles/compile/files/uberjar.ps1
@@ -1,5 +1,5 @@
 . "C:\cider-ci\setup\tmp\run-command.ps1"
 
 RunCommand -file "C:\cider-ci\bin\lein.bat" `
-           -arguments "compile" `
+           -arguments "uberjar" `
            -workingdir "C:\cider-ci\executor"
