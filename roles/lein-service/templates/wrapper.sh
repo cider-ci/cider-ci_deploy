0d35dbe6a083e82ca907cb9d33512dcedd359675
Thomas Schank <DrTom@schank.ch>
2015-12-30 23:02:47 +0100

    Clean and (pre-) compile lein services

--- roles/lein-service/templates/wrapper.sh
+++ roles/lein-service/templates/wrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p {{lein_service_log_dir}}
 chown -R {{lein_service_user}} {{lein_service_log_dir}}
-sudo -u {{lein_service_user}} -n -i {{lein_service_bin}}
+# sudo -u {{lein_service_user}} -n -i {{lein_service_bin}}
+su {{lein_service_user}} -c {{lein_service_bin}}
