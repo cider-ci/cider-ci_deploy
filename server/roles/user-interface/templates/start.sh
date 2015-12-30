89c1afa707aad5f20bd7a1023f574e72bb29a817
Thomas Schank <DrTom@schank.ch>
2015-12-30 23:02:13 +0100

    Set JAVA_OPTS instead of JVM_OPTS

--- server/roles/user-interface/templates/start.sh
+++ server/roles/user-interface/templates/start.sh
@@ -10,9 +10,6 @@ export RAILS_ENV={{rails_env}}
 export RAILS_RELATIVE_URL_ROOT={{web_sub_path}}{{web_ui_prefix}}
 
 # Memory Settings
-export JVM_OPTS="-Xmx{{user_interface_xmx_value}}"
-# it seems that the above is ignored,
-# we could either invoke `jruby -J-Xmx4G` or set JRUBY_OPTS
 export JRUBY_OPTS="-J-Xms{{user_interface_xms_value}}  -J-Xmx{{user_interface_xmx_value}}"
 
 bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:8880 \
