82b3ff8097ca55fa461f44ce2a1d1d8725e62f26
Thomas Schank <DrTom@schank.ch>
2016-05-30 19:27:35 +0200

    Add ruby-install trait

---
+++ traits/ruby-install/files/ruby-install-0.6.0/test/functions_tests/apply_patches_test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+. ./test/helper.sh
+. ./share/ruby-install/functions.sh
+
+ruby_src_dir="ruby-1.9.3-p448"
+patches=("$src_dir/$ruby_src_dir/falcon-gc.diff")
+
+function setUp()
+{
+	mkdir -p "$src_dir/$ruby_src_dir"
+	echo "diff -Naur $ruby_src_dir.orig/test $ruby_src_dir/test
+--- $ruby_src_dir.orig/test 1970-01-01 01:00:00.000000000 +0100
++++ $ruby_src_dir/test  2013-08-02 20:57:08.055843749 +0200
+@@ -0,0 +1 @@
++patch
+" > "${patches[0]}"
+}
+
+function test_apply_patches()
+{
+	cd "$src_dir/$ruby_src_dir"
+	apply_patches >/dev/null
+	cd $OLDPWD
+
+	assertTrue "did not apply downloaded patches" \
+		   '[[ -f "$src_dir/$ruby_src_dir/test" ]]'
+}
+
+function tearDown()
+{
+	rm -r "$src_dir/$ruby_src_dir"
+}
+
+SHUNIT_PARENT=$0 . $SHUNIT2
