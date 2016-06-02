9619e6ce293c6b2a10be1ac0b9a0dc94f394560f
Thomas Schank <DrTom@schank.ch>
2016-06-02 15:12:25 +0200

    Fix rabbitmq trait management port

--- traits/rabbitmq/templates/rabbitmq_env.sh
+++ traits/rabbitmq/templates/rabbitmq_env.sh
@@ -4,4 +4,4 @@ export 'RABBITMQ_NODE_PORT'='{{ci_trait_rabbitmq_node_port}}'
 export 'RABBITMQ_USER'='{{ci_exec_user}}'
 export 'RABBITMQ_VHOST'='{{ci_exec_user}}'
 export 'RABBITMQ_PASSWORD'='{{ci_exec_user}}'
-export 'RABBITMQ_MANAGEMENT_PORT'=15672
+export 'RABBITMQ_MANAGEMENT_PORT'='{{ci_trait_rabbitmq_mangement_port}}'
