61fb79d146234c0a7774c825608ec385d82a4352
Thomas Schank <DrTom@schank.ch>
2016-08-30 21:51:39 +0200

    Upgrade Ansible version to 2.1.1.0

--- bin/quick-install.sh
+++ bin/quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-ANSIBLE_VERSION=2.1.0.0
+ANSIBLE_VERSION=2.1.1.0
 CIDER_CI_DIR=/tmp/cider-ci
 
 ################################################################################
