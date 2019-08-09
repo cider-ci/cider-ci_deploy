65de6921c9264dc8b90e2fe0715ec823d10aa472
Thomas Schank <DrTom@schank.ch>
2019-08-09 12:28:42 +0200

    General update

--- traits/postgresql/templates/pgenv.sh
+++
@@ -1,4 +0,0 @@
-export 'PGUSER'='{{ci_executor_trait_postgresql_exec_user}}'
-export 'PGPORT'='{{ci_executor_trait_postgresql_port}}'
-export 'PGPASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
-export 'PGDATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
---
+++ traits/postgresql9/templates/pgenv.sh
@@ -0,0 +1,4 @@
+export 'PG9USER'='{{ci_executor_trait_postgresql_exec_user}}'
+export 'PG9PORT'='{{ci_executor_trait_postgresql_port}}'
+export 'PG9PASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
+export 'PG9DATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
