876bce3049409f9ca7d4d3dc5791e433cfb34706
Thomas Schank <DrTom@schank.ch>
2020-10-20 20:01:09 +0200

    Upgrades

---
+++ traits/postgresql1x/templates/pgenv.sh
@@ -0,0 +1,9 @@
+export 'PG{{ci_executor_trait_postgresql_version_major}}USER'='{{ci_executor_trait_postgresql_exec_user}}'
+export 'PG{{ci_executor_trait_postgresql_version_major}}PORT'='{{ci_executor_trait_postgresql_port}}'
+export 'PG{{ci_executor_trait_postgresql_version_major}}PASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
+export 'PG{{ci_executor_trait_postgresql_version_major}}DATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
+
+export 'PGUSER'='{{ci_executor_trait_postgresql_exec_user}}'
+export 'PGPORT'='{{ci_executor_trait_postgresql_port}}'
+export 'PGPASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
+export 'PGDATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
