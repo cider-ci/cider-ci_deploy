a92e644c9845b0a408582e1514c7ee15b25b8997
Thomas Schank <DrTom@schank.ch>
2022-11-22 18:06:24 +0100

    WIP cleanup and traits

--- traits/mysql/templates/mysql_env.sh
+++
@@ -1,2 +0,0 @@
-export 'MYSQL_USER'='{{ci_executor_exec_user}}'
-export 'MYSQL_PASSWORD'='{{ci_executor_exec_user}}'
--- traits/postgresql9/templates/pgenv.sh
+++
@@ -1,4 +0,0 @@
-export 'PG9USER'='{{ci_executor_trait_postgresql_exec_user}}'
-export 'PG9PORT'='{{ci_executor_trait_postgresql_port}}'
-export 'PG9PASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
-export 'PG9DATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
