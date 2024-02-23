f4be71be3227056b0fbceb5d3f959346441cc3f2
Thomas Schank <DrTom@schank.ch>
2024-02-23 14:59:50 +0100

    Some updates and fixes for Debian 12

--- traits/postgresql1x/templates/pgenv.sh
+++ traits/postgresql1x/templates/pgenv.sh
@@ -3,7 +3,11 @@ export 'PG{{ci_executor_trait_postgresql_version_major}}PORT'='{{ci_executor_tra
 export 'PG{{ci_executor_trait_postgresql_version_major}}PASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
 export 'PG{{ci_executor_trait_postgresql_version_major}}DATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
 
+{% if ci_executor_trait_postgresql_is_pg_default  %}
+
 export 'PGUSER'='{{ci_executor_trait_postgresql_exec_user}}'
 export 'PGPORT'='{{ci_executor_trait_postgresql_port}}'
 export 'PGPASSWORD'='{{ci_executor_trait_postgresql_exec_user_password}}'
 export 'PGDATABASE'='{{ci_executor_trait_postgresql_exec_user}}'
+
+{% endif %}
